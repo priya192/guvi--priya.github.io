50bee8dcd78675747914d995b9b0058011fde91e
priya192 <35070054+priya192@users.noreply.github.com>
2018-01-04 16:48:19 +0530

    Update check.py

--- check.py
+++ check.py
@@ -1,7 +1,8 @@
-num = float(input("Enter a number: "))
-if num > 0:
-   print("Positive number")
-elif num == 0:
+num = float(input("enter num:"))
+if num>0:
+   print("Positive")
+elif num==0:
    print("Zero")
+  
 else:
-   print("Negative number")
+   print("Negative")
