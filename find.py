918c190e9b65b75ed3a958b756289737e188af41
priya192 <35070054+priya192@users.noreply.github.com>
2018-01-04 17:11:43 +0530

    Update find.py

--- find.py
+++ find.py
@@ -1,1 +1,10 @@
-
+a = int(input("enter 1st no"))
+b = int(input("enter 1st no"))
+c = int(input("enter 1st no"))
+print("largest no. among three is:")
+if a>b and a>c:
+  print(a)
+elif b>a and b>c:
+  print(b)
+else:
+  print(c)
