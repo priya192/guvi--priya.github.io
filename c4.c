ac5c1b5cea31512cc7c73d62cab2b9ae9c2dd3ec
priya192 <35070054+priya192@users.noreply.github.com>
2018-01-04 17:07:39 +0530

    Update c4.c

--- c4.c
+++ c4.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
- 
 int main()
 {
-  char ch;
-  printf("\n Please Enter any character \n");
+ char ch;
+  printf("\n enter a character \n");
   scanf("%c", &ch);
   
   if( (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') )
