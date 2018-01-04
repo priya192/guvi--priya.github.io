4d0ea993b3f53ef24fdd1845fa6117343d9ba0a7
priya192 <35070054+priya192@users.noreply.github.com>
2018-01-04 17:18:17 +0530

    Update leap.py

--- leap.py
+++ leap.py
@@ -1,1 +1,9 @@
-
+def leapyr(n):
+    if n%4==0 and n%100!=0:
+        if n%400==0:
+            print("leap year")
+    elif n%4!=0:
+        print("leap year")
+  
+a = input("enter any year")
+print leapyr(a)
