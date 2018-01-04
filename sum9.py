142cf20738295279f9aa6c82b803fde44441622d
priya192 <35070054+priya192@users.noreply.github.com>
2018-01-04 17:56:30 +0530

    Update sum9.py

--- sum9.py
+++ sum9.py
@@ -1,1 +1,13 @@
+n = int(input("enter array size"))
+k = int(input("enter any no."))
+arr = []
+sum=0
+for i in range(1,n+1):
+    arr.append(int(input("")))
 
+print(arr)
+for i in range(0,k):
+    sum += arr[i]
+
+print(sum)
+    
