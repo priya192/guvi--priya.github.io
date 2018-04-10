6567e000e735b0ad914555618133ba5dd9f2fd80
priya192 <35070054+priya192@users.noreply.github.com>
2018-04-10 13:55:44 +0530

    Create words.c

---
+++ words.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#inclide<string.h>
+int main()
+{
+char a[100];
+scanf("%d",&a);
+int i=0, c=0;
+while(a[i] != null){ if(a[i]!= " " || a[i]==null)
+  c++;}
+printf("%d",c);
+return 0;
+}
