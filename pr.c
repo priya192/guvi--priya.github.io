e95e198a8a7ed6576f1f58e909dda23bfed1e37a
priya192 <35070054+priya192@users.noreply.github.com>
2018-04-10 08:30:50 +0530

    Update pr.c

--- pr.c
+++ pr.c
@@ -1,22 +1,13 @@
 #include<stdio.h>
 int main()
 {
-  int n, i, f=0;
-  scanf("%d",&n);
-  for(i=2;i<=n/2;i++)
+  int i, n1, n2;
+  scanf("%d%d",&n1&n2);
+  for(i=n1;i<=n2;i++)
   {
-  if(n%i == 0)
-  f = 1;
-  break;
+    if(i%2 !=0)
+    printf("%d",i);
   }
-if(f==1)
-{
-printf("no");
-}
-else
-{
-printf("yes");
-}
 
 return 0;
 }
