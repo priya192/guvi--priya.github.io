12bc2e5ce77ad03ab9d4207f338cc273e940cc30
priya192 <35070054+priya192@users.noreply.github.com>
2018-04-10 15:12:00 +0530

    Create vvv.c

---
+++ vvv.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[100];
+scanf("%d",&a);
+int i=0, c=0;
+while(a[i] != null)
+{ for(i=0;i<=9;i++)
+{
+if(a[i]==i)
+  c++;
+}
+}
+printf("%d",c);
+return 0;
+}
