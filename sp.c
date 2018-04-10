bb0af880129331d573ab739dc1fb827d3d7087a8
priya192 <35070054+priya192@users.noreply.github.com>
2018-04-10 15:42:54 +0530

    Create sp.c

---
+++ sp.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[100];
+scanf("%[^\n]s",a);
+int i=0, c=0;
+while(a[i] != '\0')
+{ 
+     
+if(a[i]==' ' || a[i]=='!' || a[i]=='@' || a[i]=='#' || a[i]=='$' || a[i]=='%' || a[i]=='^' || a[i]=='&' || a[i]=='*' || a[i]=='~')
+  c = c+1;
+    i++;
+}
+printf("%d",c);
+return 0;
+}
