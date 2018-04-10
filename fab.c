92f447da45ab6a4eaefad3ba78bcb66bb5f3fd15
priya192 <35070054+priya192@users.noreply.github.com>
2018-04-10 15:56:38 +0530

    Update fab.c

--- fab.c
+++ fab.c
@@ -2,10 +2,10 @@
 #include<string.h>
 int main()
 {
-int a=0, b=1, n;
+int a=1, b=1, n;
 scanf("%d",&n);
 int i, c;
-printf("%a %b",a,b);
+if(n>=2){printf("%a %b",a,b);}
 for(i=0;i<n;i++)
 {
   c = a + b;
