ff2c13faab5aa08048dd4258bde19cf0911d16d4
priya192 <35070054+priya192@users.noreply.github.com>
2018-04-10 15:27:49 +0530

    Create one.c

---
+++ one.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[100];
+scanf("%[^\n]s",a);
+int i=0, c=0;
+while(a[i] != '\0')
+{ 
+    if(a[i]!=' ')
+  c = c+1;
+    i++;
+}
+printf("%d",c);
+return 0;
+}
