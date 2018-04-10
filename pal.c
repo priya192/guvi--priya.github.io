6d254b2526c5d2d924108d7176ba5b5731faee52
priya192 <35070054+priya192@users.noreply.github.com>
2018-04-10 08:20:15 +0530

    Update pal.c

--- pal.c
+++ pal.c
@@ -1,1 +1,22 @@
-
+#include<stdio.h>
+int main()
+{
+  int n,s=0, i;
+  scanf("%d",&n);
+  if(n<=1000)
+  {temp = n;
+   while(n)
+   { r = n%10;
+       s = s*10 + r;
+        n = n/10; }
+  }
+  if(temp == sum)
+  {
+   printf("yes"); 
+  }
+  else
+  {
+    printf("not");
+  }
+ return 0; 
+}
