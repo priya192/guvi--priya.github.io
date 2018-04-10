02b0c86b64835d6080b9e0100369501e891f88b8
priya192 <35070054+priya192@users.noreply.github.com>
2018-04-10 15:41:00 +0530

    Update line.c

--- line.c
+++ line.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
-#inclide<string.h>
+#include<string.h>
 int main()
 {
 char a[100];
-scanf("%d",&a);
+scanf("%s",a);
 int i=0, l=0;
 while(a[i] != null)
 { if(a[i]== "." || a[i]=="?")
   l++;
+ i++;
 }
 printf("%d",l);
 return 0;
