31ea81b2ec7da655c06ea21dac96c43dbb103f71
priya192 <35070054+priya192@users.noreply.github.com>
2018-04-16 09:31:27 +0530

    Create r5.c

---
+++ r5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+
+   int n ;
+   scanf("%d",&n);
+
+while(n)
+{
+    if(n%2==0)
+   { printf("%d",n);
+   break;
+   }
+    
+    n--;
+}
+
+ return 0;
+
+}
